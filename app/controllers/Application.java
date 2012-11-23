317400679790eb065b7ef766c42f6277bb89fb9f
Ketan Soni <ketansony@gmail.com>
2012-11-23 12:10:05 +0530

    Argha/Ketan - Back end order fulfillment app which talks to Rails app

---
+++ app/controllers/Application.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import models.Order;
+import org.codehaus.jackson.JsonNode;
+import play.libs.Json;
+import play.libs.WS;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+  
+  public static Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
+
+  public static Result getStatus(String orderId){
+      String url_order = "http://localhost:3000/order/show/"+orderId;
+      WS.Response promise = WS.url(url_order).get().get();
+      Order orderObject;
+      try{
+          JsonNode order = promise.asJson();
+          orderObject = Json.fromJson(order,Order.class);
+      }catch (RuntimeException e)
+      {
+          return ok(views.html.failure.render());
+      }
+
+      return ok(views.html.order.render(orderObject));
+  }
+
+  public static Result sendOrder(String orderId){
+    return ok("success");
+  }
+  public static Result update(String orderId){
+      String url_order = "http://localhost:3000/order/update/"+orderId;
+      WS.Response promise = WS.url(url_order).post("status=Delivered").get();
+
+      return ok(views.html.updated_message.render());
+  }
+
+}
---
+++ app/models/Order.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Order {
+    public String getStatus() {
+        return status;
+    }
+
+    private String status;
+
+    public String getAddress() {
+        return address;
+    }
+
+    private String address;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    private int quantity;
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    private String item_name;
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    private String order_id;
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    private String customer_name;
+
+}
